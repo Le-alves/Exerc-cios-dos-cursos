c690b07bc0cb168e3021ee6a7d753b7cc2905dc8
Lê Alves <leticiaviz.alves@gmail.com>
2022-02-22 11:30:55 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,8 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Oi Bernardo!");
-            Console.WriteLine("Vou invadir teu sistema!");
+            ;
         }
     }
 }
